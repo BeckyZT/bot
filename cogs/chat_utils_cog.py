522be40dcfaad67467c375a516e1adfc4cb6c3e1
Rebeca <rebecapionk@hotmail.com>
2020-09-20 22:08:25 -0300

    chat utils cog

---
+++ cogs/chat_utils_cog.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+from unidecode import unidecode
+
+
+class ChatUtilsCog(commands.Cog, name='Chat Utils'):
+
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		text = unidecode(message.content.lower())
+		distract = ['distrai', 'distraido']
+
+		if any(d for d in distract if d in text):
+			await message.channel.send('<a:distract:757381529478496346>')
+
+	@commands.command()
+	async def pong(self, ctx):
+		await ctx.send('Ué')
+
+	@commands.command()
+	async def ping(self, ctx):
+		await ctx.send('Pong :ping_pong:')
+
+
+def setup(bot):
+	bot.add_cog(ChatUtilsCog(bot))
