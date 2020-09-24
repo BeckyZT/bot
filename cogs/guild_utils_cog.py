d26cdc9482b6a8d110aa3a2b7cd1510b65dc92e2
Mateus Zanatta Bergamin <mateuszanatta1@gmail.com>
2020-09-24 01:58:11 -0300

    Alterado para comando react aceitar multiplos emojis/cargos

--- cogs/guild_utils_cog.py
+++ cogs/guild_utils_cog.py
@@ -1,8 +1,11 @@
+from typing import Union
+
 from discord import TextChannel, PartialEmoji, Role
 from discord.ext import commands
+from discord.ext.commands import Greedy
 
 from db import Db
-from db.models import RoleReaction,Config
+from db.models import RoleReaction, Config
 
 
 class GuildUtilsCog(commands.Cog, name='Guild Utils'):
@@ -19,7 +22,7 @@ class GuildUtilsCog(commands.Cog, name='Guild Utils'):
 
 	@commands.Cog.listener()
 	async def on_raw_reaction_add(self, payload):
-		role_reactions = RoleReaction.get(self.session,payload.channel_id,payload.message_id,str(payload.emoji))
+		role_reactions = RoleReaction.get(self.session, payload.channel_id, payload.message_id, str(payload.emoji))
 		roles = []
 		guild = self.bot.get_guild(payload.guild_id)
 		for rr in role_reactions:
@@ -38,8 +41,8 @@ class GuildUtilsCog(commands.Cog, name='Guild Utils'):
 		await guild.get_member(payload.user_id).remove_roles(*roles)
 
 	@commands.Cog.listener()
-	async def on_raw_message_delete(self,payload):
-		role_reactions = RoleReaction.get(self.session,payload.channel_id,payload.message_id)
+	async def on_raw_message_delete(self, payload):
+		role_reactions = RoleReaction.get(self.session, payload.channel_id, payload.message_id)
 
 		if len(role_reactions) > 0:
 			for rr in role_reactions:
@@ -47,16 +50,38 @@ class GuildUtilsCog(commands.Cog, name='Guild Utils'):
 			self.session.commit()
 
 	@commands.command()
-	async def react(self, ctx, channel: TextChannel, message_id, emoji: PartialEmoji, role:Role):
+	async def react(self, ctx, channel: TextChannel, message_id, args: Greedy[Union[PartialEmoji, Role]]):
 		message = await channel.fetch_message(message_id)
 
-		if len(RoleReaction.get(self.session,channel.id,message.id,str(emoji),role.id)) > 0:
-			await ctx.send('Já existente')
-		else:
-			role_reaction = RoleReaction(channel_id=channel.id,message_id=message.id,emoji=str(emoji),role_id=role.id)
-			self.session.add(role_reaction)
-			self.session.commit()
-			await message.add_reaction(emoji)
+		roles = []
+		emojis = []
+
+		for i in range(len(args)):
+			if i % 2 == 0:
+				if type(args[i]) != PartialEmoji:
+					await ctx.send(f'Inválido, formato: <Emoji> <Cargo>')
+					return
+				else:
+					emojis.append(args[i])
+			else:
+				if type(args[i]) != Role:
+					await ctx.send(f'Inválido, formato: <Emoji> <Cargo>')
+					return
+				else:
+					roles.append(args[i])
+
+		if len(roles) != len(emojis):
+			await ctx.send("Quantia de Roles/Emojis é diferente, operação abortada")
+			return
+
+		for role,emoji in zip(roles, emojis):
+			if len(RoleReaction.get(self.session,channel.id,message.id,str(emoji),role.id)) > 0:
+				await ctx.send('Já existente')
+			else:
+				role_reaction = RoleReaction(channel_id=channel.id,message_id=message.id,emoji=str(emoji),role_id=role.id)
+				self.session.add(role_reaction)
+				self.session.commit()
+				await message.add_reaction(emoji)
 
 	@commands.command()
 	async def unreact(self,ctx, channel: TextChannel, message_id,emoji: PartialEmoji=None):
